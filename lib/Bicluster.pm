e58abc5ec8f7903420446b1bb0db051b138dca4e
Emmanuel Di Pretoro <edipretoro@gmail.com>
2009-07-03 21:53:15 +0200

    Bicluster.pm: correct some bugs and add accessors

--- lib/Bicluster.pm
+++ lib/Bicluster.pm
@@ -7,13 +7,38 @@ sub new {
     my ($class, %args) = @_;
     my $self = {};
 
-    $self->{left} = $args{left} || 'None';
-    $self->{right} = $args{right} || 'None';
-    $self->{vec} = $args{vec};
-    $self->{distance} = $args{distance} || 0.0;
-    $self->{id} = $args{id} || 'None';
+    $self->{left} = defined $args{left} ? $args{left} : 'None';
+    $self->{right} = defined $args{left} ? $args{right} : 'None';
+    $self->{vec} = defined $args{vec} ? $args{vec} : [];
+    $self->{distance} = defined $args{distance} ? $args{distance} : 0.0;
+    $self->{id} = defined $args{id} ? $args{id} : 'None';
 
     bless $self, $class;
+}
+
+sub vec {
+    my $self = shift;
+    return $self->{vec};
+}
+
+sub id {
+    my $self = shift;
+    return $self->{id};
+}
+
+sub left {
+    my $self = shift;
+    return $self->{left};
+}
+
+sub right {
+    my $self = shift;
+    return $self->{right};
+}
+
+sub distance {
+    my $self = shift;
+    return $self->{distance};
 }
 
 1;
